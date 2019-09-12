7395f2567458a30d7f4352e0ab3c78cd8ff92ee5
mayconbj15 <mayconbj15@gmail.com>
2019-09-11 22:44:07 -0300

    Colocando diretório Games correto

--- Games/app/src/main/java/com/example/games/MainActivity.java
+++ Games/app/src/main/java/com/example/games/MainActivity.java
@@ -2,46 +2,61 @@ package com.example.games;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity{
-    private ImageView imgBichos;
-    private ImageView imgAdivinha;
-    private Button button;
+import com.example.games.Adivinha;
+import com.example.games.Bichos;
+import com.example.games.R;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgBichos = findViewById(R.id.imageViewBichos);
-        imgBichos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Bichos.class));
-            }
-        });
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        imgAdivinha = findViewById(R.id.imageViewAdivinha);
-        imgAdivinha.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Adivinha.class));
-            }
-        });
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
 
-        button = findViewById(R.id.buttonSide);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, SideBar.class));
-            }
-        });
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        int id = menuItem.getItemId();
+        if (id == R.id.AnimaisId) {
+            startActivity(new Intent(MainActivity.this, Bichos.class));
+            return true;
+        } else if (id == R.id.AdivinhaId) {
+            startActivity(new Intent(MainActivity.this, Adivinha.class));
+            return true;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 }
