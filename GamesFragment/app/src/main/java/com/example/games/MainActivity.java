79f8ebd9043b78892e8c2ad0284719ffe80adb30
mayconbj15 <mayconbj15@gmail.com>
2019-09-14 16:18:25 -0300

    Resolvendo as transições de Fragments

--- GamesFragment/app/src/main/java/com/example/games/MainActivity.java
+++ GamesFragment/app/src/main/java/com/example/games/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.games;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -13,17 +13,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import com.example.games.Adivinha;
-import com.example.games.Bichos;
-import com.example.games.R;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
     private DrawerLayout drawerLayout;
+
     private NavigationView navigationView;
 
 
@@ -41,37 +38,63 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        /** inicializa uma fragment vazia na tela inicial **/
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(
+                    R.id.fragment, new BlankFragment()).commit();
+        }
+
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         // Handle navigation view item clicks here.
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment fragment = null;
+        Class fragmentClass = null;
 
-        boolean success = false;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         int id = menuItem.getItemId();
 
         if (id == R.id.AnimaisId) {
-            getSupportFragmentManager().beginTransaction().add(
-                    R.id.fragment, new FragmentAnimais()
-            ).commit();
-
-            success = true;
+            fragmentClass = FragmentAnimais.class;
         } else if (id == R.id.AdivinhaId) {
-            getSupportFragmentManager().beginTransaction().add(
-                    R.id.fragment2, new FragmentAdivinha()
-            ).commit();
-
-
-
-            success = true;
+            fragmentClass = FragmentAdivinha.class;
         }
+
+        try{
+            fragment = (Fragment) fragmentClass.newInstance();
+        }catch (IllegalAccessException e){
+            e.printStackTrace();
+        }catch (InstantiationException e){
+            e.printStackTrace();
+        }
+
+        clearActivity();
+        fragmentManager.beginTransaction().replace(
+                R.id.fragment, fragment).commit();
+
         drawerLayout.closeDrawer(GravityCompat.START);
 
-        return success;
+        return true;
+
+
+    }
+
+    public void removeFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().remove(fragment);
+    }
+
+    public void clearActivity(){
+        TextView textView = findViewById(R.id.mainText);
+        TextView textView2 = findViewById(R.id.messageText);
+        ImageView imageView = findViewById(R.id.imageViewNerd);
+
+        textView.setVisibility(View.INVISIBLE);
+        textView2.setVisibility(View.INVISIBLE);
+        imageView.setVisibility(View.INVISIBLE);
     }
 }
